97c532be0bbb0a97484c9874428e462a00bdd971
Daviddoan14122k <doanchuong14122k@gmail.com>
2021-10-27 08:39:53 +0700

    submit practical 13

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,87 +1,3 @@
-package vn.edu.usth.weather;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.util.Log;
-
-public class WeatherActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weather);
-        Log.i("Chuong", "onCreate");
-//        ForecastFragment firstFragment = new ForecastFragment();
-//        getSupportFragmentManager().beginTransaction().add(R.id.container,firstFragment).commit();
-//        WeatherFragment secondFragement =new WeatherFragment();
-//        getSupportFragmentManager().beginTransaction().add(R.id.container,secondFragement).commit();
-        PagerAdapter adapter = new AdapterPage(getSupportFragmentManager());
-        ViewPager pager = (ViewPager) findViewById(R.id.pager);
-//        pager.setOffscreenPageLimit(3);
-        pager.setAdapter(adapter);
-
-	# add play music
-	MediaPlayer play = MediaPlayer.create(getApplicationContext(), R.raw.audiomusic);
-        play.start();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.action_items, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.refresh:
-                Toast toast = Toast.makeText(this, "Refresh", Toast.LENGTH_SHORT);
-                toast.show();
-                super.onRestart();
-
-            case R.id.settings:
-                Intent intent = new Intent(this, PrefActivity.class);
-                startActivity(intent);
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.i("Chuong", "onRestart");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i("Chuong","onDestroy");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.i("Chuong","onPause");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i("Chuong","onStart");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i("Chuong","onStop");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i("Chuong","onResume");
-    }
-
     final Handler ProgressHandlerMessage = new Handler(Looper.getMainLooper()) {
 
         public void handleMessage(Message msg) {
@@ -91,40 +7,82 @@ public class WeatherActivity extends AppCompatActivity {
         }
     };
 
+    // Practical work 13
     @SuppressLint("StaticFieldLeak")
     public void Refresh() {
-        // Practical work 13
 
-        final Handler handler = new Handler(Looper.getMainLooper()) {
+//        final Handler handler = new Handler(Looper.getMainLooper()) {
+//            @Override
+//            public void handleMessage(Message msg) {
+//// This method is executed in main thread
+//                String content = msg.getData().getString("Server_Response");
+//                Toast toast = Toast.makeText(getApplicationContext(), content, Toast.LENGTH_SHORT);
+//                toast.show();
+//            }
+//        };
+//        Thread th = new Thread(() -> {
+//// this method is run in a worker thread
+//            try {
+//// wait for 5 seconds to simulate a long network access
+//                int i;
+//                for (i=0;i<10;i++){
+//                    Log.i(mess,"loop"+i);
+//                    Thread.sleep(1000);
+//                }
+//
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//// Assume that we got our data from server
+//            Bundle bundle = new Bundle();
+//            bundle.putString("Server_Response", "Refreshing Start");
+//// notify main thread
+//            Message msg = new Message();
+//            msg.setData(bundle);
+//            handler.sendMessage(msg);
+//        });
+//        th.start();
+
+        // Practical work 14
+        AsyncTask<Integer, Integer, Integer> taskA = new AsyncTask<Integer, Integer, Integer>() {
+
             @Override
-            public void handleMessage(Message msg) {
-// This method is executed in main thread
-                String content = msg.getData().getString("Server_Response");
-                Toast toast = Toast.makeText(getApplicationContext(), content, Toast.LENGTH_SHORT);
+            protected void onPreExecute() {
+//                String content = "Starting delay Android";
+                Toast toast = Toast.makeText(getApplicationContext(), "Starting delay Android", Toast.LENGTH_SHORT);
                 toast.show();
             }
-        };
-        Thread th = new Thread(() -> {
-// this method is run in a worker thread
-            try {
-// wait for 5 seconds to simulate a long network access
-                int i;
-                for (i=0;i<10;i++){
-                    Log.i(mess,"loop"+i);
-                    Thread.sleep(1000);
-                }
 
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            @Override
+            protected void onPostExecute(Integer integer) {
+//                String content = "Finished delay Android";
+                Toast toast = Toast.makeText(getApplicationContext(),  "Finished delay Android", Toast.LENGTH_SHORT);
+                toast.show();
             }
-// Assume that we got our data from server
-            Bundle bundle = new Bundle();
-            bundle.putString("Server_Response", "Refreshing Start");
-// notify main thread
-            Message msg = new Message();
-            msg.setData(bundle);
-            handler.sendMessage(msg);
-        });
-        th.start();
+
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+//                String content = "Uppdate  Android";
+                Toast toast = Toast.makeText(getApplicationContext(), "Uppdate  Android",+ values[0]);
+                toast.show();
+            }
+            @SuppressLint("StaticFieldLeak")
+            @Override
+            protected Integer doInBackground(Integer... integers) {
+                try {
+// wait for 5 seconds to simulate a long network access
+                    int i;
+                    for (i = 0; i < 10; i++) {
+//                    Log.i(mess,"loop"+i);
+                        Thread.sleep(1000);
+                        ProgressHandlerMessage.sendEmptyMessage(i);
+                    }
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return 0;
+            }
+        };
     }
 }
